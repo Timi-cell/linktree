6d091b27100e664e39070e5acefde3768f557d6e
Timi-cell <adedayotimilehin10@gmail.com>
2022-10-21 06:00:53 -0700

    created the boilerplate for my linktree react app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./components/NotFound";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
