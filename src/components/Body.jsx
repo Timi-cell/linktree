d4752e51ac880c61cb913d1abbd66b4ce57525c7
Timi-cell <adedayotimilehin10@gmail.com>
2022-10-21 13:42:47 -0700

    created components and finished the header section for desktop view

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Body = () => {
+    return ( 
+        <div className='body'>
+            
+        </div>
+     );
+}
+ 
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="./avatar.JPEG" alt="profile__img" id="profile__img" />
+      <h1 id="name">Oluwa Timilehin</h1>
+    </div>
+  );
+};
+
+export default Header;
