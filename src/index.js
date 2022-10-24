0c5dc4b04b9df0c50cf9e9a5f992bb6c07d11f92
Timi-cell <adedayotimilehin10@gmail.com>
2022-10-24 15:01:05 -0700

    deleted some dependencies

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from "./components/NotFound";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <div>
+    <App />
+  </div>
 );
